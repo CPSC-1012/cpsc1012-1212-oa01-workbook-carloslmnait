3599452921d30d470aaee508629799009f392f31
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-10 15:37:49 -0700

    TwoLinesApp

---
+++ TwoLinesApp/TwoLinesApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TwoLines
+{
+    class TwoLines
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programming is fun!");
+            Console.WriteLine("I can't get enough of it!");
+        }
+    }
+}
