01ec5313692b1526ef52fca82a5c7dd037b4d7a0
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-10 16:07:23 -0700

    First Commit

---
+++ IntegerVariablesApp/IntegerVariablesApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace IntegersVariablesApp
+{
+    class IntegerVariablesApp
+    {
+        static void Main(string[] args)
+        {
+            int checking; // Declare an int variable named checking
+            long days; // Declare a long variable named days
+            checking = -20;
+            days = 189000;
+            Console.WriteLine($"Our account balance is ${checking}");
+            Console.WriteLine($"About {days} days ago Columbus stood on this spot.");
+        }
+    }
+}
---
+++ Variable.cs/Variable.cs/Program.cs
@@ -0,0 +1,8 @@
+﻿
+static void Main(string[] args)
+{
+    int intValue;
+    intValue = 5;
+    Console.Write("This value is ");
+    Console.WriteLine(intValue);
+}
