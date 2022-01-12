2e45812628bdaba7a59a154ca627db2a3acd2068
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-12 16:08:21 -0700

    SalesActivity

--- Sales.cs/Sales.cs/Program.cs
+++ Sales.cs/Sales.cs/Program.cs
@@ -1,16 +1,51 @@
-﻿namespace SalesApp
+﻿/* Purpose:     Compute the total of a sales item using the price and tax.
+ * 
+ * Inputs:      price in dollars and cents
+ * 
+ * Outputs:     sales total
+ * 
+ * Algorithm:   Step 1)Prompt and read in the sales price
+ *              Step 2) Calculate the sale total using the formula:
+ *                          salesTotal = salePrice + salesTax
+ *                      where salesTax = salePrice * 0.05
+ *              Step 3) Display the computed sale total
+ *              
+ * Last modified:       2022-01-12
+ * */
+namespace SaleApp
 {
-    class SalesApp
+    internal class Program
     {
         static void Main(string[] args)
         {
-            double price, tax, total;
-            price = 29.75;
-            tax = 1.76;
-            total = 31.51;
-            Console.WriteLine("The price of the item is " + price);
-            Console.WriteLine("The tax is " + tax);
-            Console.WriteLine("The total is " + total);
+            // Declare variables for inputs and outputs
+            double salePrice;
+            const double saleTaxRate = 0.05;
+            double saleTax;
+            double saleTotal;
+            string userInput;
+
+            //Step 1)Prompt and read in the sales price
+            Console.Write("Enter the sale price:");
+            userInput = Console.ReadLine();
+
+            //Convert userInput from a string type to a double type
+            salePrice = double.Parse(userInput);
+
+            //Step 2) Calculate the sale total
+            saleTax = salePrice * saleTaxRate;
+            saleTotal = salePrice + saleTax;
+
+            //Step 3) Display the computed sale total
+            /*          1234567890  12345678
+             *          Price:        $29.75
+             *          Tax:           $1.76
+             *          Total:        $31.51
+             * */
+            Console.WriteLine($"{"Price:",-10} {salePrice, 8:C}");
+            Console.WriteLine($"{"Tax:",-10} {saleTax,8:C}");
+            Console.WriteLine($"{"Total:",-10} {saleTotal,8:C}");
+
         }
     }
 }
