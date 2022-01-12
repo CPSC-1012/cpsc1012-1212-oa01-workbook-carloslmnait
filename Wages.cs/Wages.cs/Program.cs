2684b891d7ba498c023d162959d5eeb3a625d96d
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-12 16:49:08 -0700

    Wages App First Commit

---
+++ Wages.cs/Wages.cs/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Purpose:     Compute the weekly wage of an employee
+ * 
+ * Inputs:      regular hourly rate
+ *              regular hours worked
+ *              overtime hourly rate
+ *              overtime hours worked
+ * 
+ * Outputs:     regular wages
+ *              overtime wages
+ * 
+ * Algorithm:   Step 1)Prompt for regular hourly rate,
+ *                      regular hours worked
+ *                      overtime hourly rate,
+ *                      overtime hours worked
+ *              Step 2) Compute the following values:
+ *                          regularWages = basePay * regularHours;
+                            overtimeWages = overtimePay * overtimeHours;
+                               totalWages = regularWages + overtimeWages;
+ *              Step 3) Display the wages for the week
+ *              
+ * Last modified:       2022-01-12
+ * */
+namespace SaleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Base Pay:");
+            double basePay = double.Parse(Console.ReadLine());
+            Console.Write("Enter the Regular Hours: ");
+            double regularHours = double.Parse(Console.ReadLine());
+            Console.Write("Enter the Overtime Pay: ");
+            double overtimePay = double.Parse(Console.ReadLine());
+            Console.Write("Enter the Overtime Hours: ");
+            double overtimeHours = double.Parse(Console.ReadLine());
+
+            double regularWages = basePay * regularHours;
+            double overtimeWages = overtimePay * overtimeHours;
+            double totalWages = regularWages + overtimeWages;
+
+            Console.WriteLine("Regular Wages for this week are $" + regularWages);
+            Console.WriteLine("Overtime Wages for this week are $" + overtimeWages);
+            Console.WriteLine("Total Wages for this week are $" + totalWages);
+
+
+        }
+    }
+}
