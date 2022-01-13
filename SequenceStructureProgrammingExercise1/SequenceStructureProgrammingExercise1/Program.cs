b15fb8885932354fa986d030783064d5e57e6a90
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-13 16:22:22 -0700

    First Problem

---
+++ SequenceStructureProgrammingExercise1/SequenceStructureProgrammingExercise1/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Purpose:     Displays the message with your name, age, and expected salary 
+ * 
+ * Inputs:      Name
+ *              Age
+ *              annualPay
+ * 
+ * Outputs:     My name is ...., my age is ...., and I hope to earn .... per year
+ * 
+ * Algorithm:   Step 1)Prompt for   name
+ *                                  age
+ *                                  annualPay
+ *
+ *              Step 3) Display the output
+ *              
+ * Last modified:       2022-01-13
+ * 
+ * Iteration 2:
+ *          string name = "Rachel Notely";
+            int age = 43;
+            double annualPay = 133_404.00;
+
+            Console.WriteLine($"My name is {name},my age is {age}, and I hope to earn ${annualPay} per year.");
+ * */
+
+namespace SequenceStructureProblem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            double annualPay;
+
+            Console.Write("Enter your name:");
+            name = Console.ReadLine();
+
+            Console.Write("Enter your age:");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your annual income:");
+            annualPay = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"My name is {name},my age is {age}, and I hope to earn ${annualPay} per year.");
+           
+        }
+    }
+}
