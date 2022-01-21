ea1868cfd2e83f1fbb718d062e1b6d2d43a5d7c0
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-20 17:04:46 -0700

    first commit

---
+++ TwoLinesApp(Practice)/TwoLinesApp(Practice)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoLinesApp_Practice_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            int Age;
+            double annualPay;
+
+            Console.WriteLine("Enter your name:");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your expected annual pay:");
+            annualPay = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Hello, my name is {Name}, my age is {Age} and I hope to earn ${annualPay} per year");
+        }
+    }
+}
