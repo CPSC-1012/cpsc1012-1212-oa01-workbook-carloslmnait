e3330a359468119e35d2be521462392a6abce217
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-20 18:10:26 -0700

    first

---
+++ ArithmeticProblem3/ArithmeticProblem3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArithmeticProblem3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideA,
+                sideB,
+                hypotenuse;
+
+            Console.WriteLine("Enter first side:");
+            sideA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second side:");
+            sideB = double.Parse(Console.ReadLine());
+
+            hypotenuse = Math.Sqrt((sideA * sideA) + (sideB * sideB));
+
+            Console.WriteLine($"The hypotenuse is {hypotenuse}");
+        }
+    }
+}
