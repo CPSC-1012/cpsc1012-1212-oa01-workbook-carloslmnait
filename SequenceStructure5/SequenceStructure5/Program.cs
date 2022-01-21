79f3ebb9f4bac2b8bd90de1eee4513deeca396b1
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-20 17:55:39 -0700

    first

---
+++ SequenceStructure5/SequenceStructure5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SequenceStructure5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double futureInvestmentValue,
+                investmentAmount,
+                monthlyInterestRate,
+                numberOfYears;
+
+            Console.WriteLine("Enter investment amount:");
+            investmentAmount = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the annual interest rate in percentage:");
+            double annualInterestRate = double.Parse(Console.ReadLine());  
+            Console.WriteLine("Enter the number of Years:");
+            numberOfYears = double.Parse(Console.ReadLine());
+
+            monthlyInterestRate = (annualInterestRate / 12 )/ 100;
+            futureInvestmentValue = investmentAmount * (Math.Pow((1 + monthlyInterestRate),(numberOfYears * 12)));
+
+            Console.WriteLine($"The future calue is ${futureInvestmentValue}");
+
+        }
+    }
+}
