cef53524157934e89e6ce0645cdd3499bb66f60c
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-20 18:14:50 -0700

    first

---
+++ ArithmeticProblem4/ArithmeticProblem4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArithmeticProblem4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sumOfDigits = 0;
+            int digit;
+            int tempNumber;
+
+            Console.Write("Enter a three digit number: ");
+            number = int.Parse(Console.ReadLine());
+            tempNumber = number;
+
+            digit = tempNumber % 10;
+            sumOfDigits += digit;
+            tempNumber = tempNumber / 10;
+
+            digit = tempNumber % 10;
+            sumOfDigits += digit;
+            tempNumber = tempNumber / 10;
+
+            digit = tempNumber % 10;
+            sumOfDigits += digit;
+
+            digit = tempNumber / 10;
+            sumOfDigits += digit;
+
+            // Display the sum 
+            Console.WriteLine($"The sum of the digits is {sumOfDigits} for {number}");
+        }
+    }
+}
