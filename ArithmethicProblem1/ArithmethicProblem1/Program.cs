901175356503e23d71f8b6341d047167845db99a
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-20 18:01:15 -0700

    first

---
+++ ArithmethicProblem1/ArithmethicProblem1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArithmethicProblem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            double  number1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            double number2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            double number3 = double.Parse(Console.ReadLine());
+
+            double mean = (number1 + number2 + number3) / 3;
+
+            Console.WriteLine($"The mean is {mean}");
+        }
+    }
+}
