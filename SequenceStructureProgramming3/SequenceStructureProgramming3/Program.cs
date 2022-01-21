28f3e1c03c148e648d196b9dc98a56457079368b
Carlos Louise Melchor <cmelchor1@nait.ca>
2022-01-20 17:18:36 -0700

    first commit

---
+++ SequenceStructureProgramming3/SequenceStructureProgramming3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SequenceStructureProgramming3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double side, area;
+
+            Console.WriteLine("Enter the side of the hexagon:");
+            side = double.Parse(Console.ReadLine());
+
+            area = ((3 * Math.Sqrt(3)) / 2) * (side * side);
+
+            Console.WriteLine($"The area of the hexagon is {area}");
+        }
+    }
+}
